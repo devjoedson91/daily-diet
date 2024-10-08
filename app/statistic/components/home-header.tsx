380210a13927bef1838160f09b0f88295714bf98
Joedson Ferreira <95484503+devjoedson91@users.noreply.github.com>
2024-10-08 15:17:36 -0300

    feat: add signOut popover

--- app/statistic/components/home-header.tsx
+++ app/statistic/components/home-header.tsx
@@ -2,25 +2,49 @@ import Image from "next/image";
 import Logo from "@/assets/logo.svg";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 
 export default function HomeHeader() {
-  const { data } = useSession();
+  const { data: session } = useSession();
+
+  function handleLogout() {
+    signOut();
+  }
 
   return (
     <Card className="flex items-center justify-between bg-transparent border-none shadow-none">
       <div className="w-24 h-10 relative">
         <Image src={Logo} alt="Logo" fill />
       </div>
-      <Avatar className="w-11 h-11 border-2 border-gray-1">
-        <AvatarImage
-          src={
-            data?.user?.image
-              ? data.user.image
-              : "https://github.com/shadcn.png"
-          }
-        />
-      </Avatar>
+      {session?.user?.image ? (
+        <Popover>
+          <PopoverTrigger>
+            <Avatar className="w-11 h-11 border-2 border-gray-1">
+              <AvatarImage src={session.user.image} />
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent className="bg-white mr-2">
+            <Button
+              className="flex items-center gap-2 w-full h-full"
+              onClick={handleLogout}
+            >
+              <LogOut size={20} />
+              <h1 className="text-lg font-bold">Sair</h1>
+            </Button>
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <Avatar className="w-11 h-11 border-2 border-gray-1">
+          <AvatarImage src="https://github.com/shadcn.png" />
+        </Avatar>
+      )}
     </Card>
   );
 }
